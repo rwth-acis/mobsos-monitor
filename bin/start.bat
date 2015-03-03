c54aeb8338a9152e3f64274a33bfb7cf1a15dc02
Dominik Renzel <dominik.renzel@gmail.com>
2015-03-03 12:42:56 +0100

    Introduced logging to file

--- bin/start.bat
+++ bin/start.bat
@@ -1,7 +1,10 @@
+@echo off
+
 cd %~dp0
 cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;%BASE%/export/jars/*;"
 
-:: tail -n10 -f %1 | java -cp %CLASSPATH% -Dorg.slf4j.simpleLogger.defaultLogLevel=debug acis.dbis.rwth.mobsos.monitor.Monitor
-cat %1 | java -cp %CLASSPATH% -Dorg.slf4j.simpleLogger.defaultLogLevel=debug acis.dbis.rwth.mobsos.monitor.Monitor
+if not exist log mkdir log
+
+tail -n0 -f %1 | java -cp %CLASSPATH% -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dorg.slf4j.simpleLogger.logFile=log\monitor.log -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat="yyyy-MM-dd HH:mm:ss" acis.dbis.rwth.mobsos.monitor.Monitor
--- bin/start.sh
+++ bin/start.sh
@@ -5,4 +5,9 @@ cd ${SCRIPTDIR}/../
 BASE=${PWD}
 export CLASSPATH="${BASE}/lib/*:${BASE}/export/jars/*"
 
-tail -n0 -f $1 | java -cp ${CLASSPATH} -Dorg.slf4j.simpleLogger.defaultLogLevel=debug acis.dbis.rwth.mobsos.monitor.Monitor
+if [ ! -d ./log ] 
+then
+    mkdir ./log
+fi
+
+tail -n0 -f $1 | java -cp ${CLASSPATH} -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dorg.slf4j.simpleLogger.logFile=log\monitor.log -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat="yyyy-MM-dd HH:mm:ss" acis.dbis.rwth.mobsos.monitor.Monitor
